606c61e5603c6d2d86a7776699956340e5ff311c
Jaskirat9 <37841179+Jaskirat9@users.noreply.github.com>
2019-02-20 15:42:47 -0500

    Update minmax.cpp

--- minmax.cpp
+++ minmax.cpp
@@ -3,24 +3,51 @@
 // 02.13.18
 //SAVED FILE
 // 
-
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
 #include <climits>
-
 using namespace std;
+int main() {
 
-int main () {
-ifstream fin ("Current_Reservoir_Levels.tsv");
-if (fin.fail()) {
-	cerr << "File cannot be opened for reading." << endl; 
-	exit(1); //exit if file failed to open
-	}
+    ifstream fin("Current_Reservoir_Levels.tsv");
+    if (fin.fail()) {
+            cerr << "File cannot be opened for reading." << endl;
+            exit(1); // exit if failed to open the file
+    }
+    double min=1000000000, max=0;
+    // skip first line
+    string junk;
+    getline(fin, junk);
 
-//skip first line
-string junk;
-getline(fin,junk);
+    //read file
+    string date, user_date;
+    double eastSt, eastEl, westSt, westEl;
 
+    //user input
+    cout << "Enter a date: ";
+    cin >> user_date;
 
-int min;
+    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl)
+    {
+        fin.ignore(INT_MAX, '\n');
+        //user check date and give the correct value for east storage
+        if(min > eastSt)
+            {
+                min = eastSt; //store min value
+            }
+        else if(max < eastSt)
+            {
+                max = eastSt; //stores max value
+            }
+        {
+
+        }
+    }
+    cout << "minimum storage in East basin:" << min << endl; //prints min
+    cout << "MAXimum storage in East basin:" << max << endl; //prints max
+return 0;
+}
+
+	
