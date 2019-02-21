d18456dcfda4dd27b4689f4249bcd4b9f7ea1f09
Jaskirat9 <37841179+Jaskirat9@users.noreply.github.com>
2019-02-20 19:40:47 -0500

    Create reverse-order.cpp

---
+++ reverse-order.cpp
@@ -0,0 +1,53 @@
+//JASKIRAT KAUR
+//CSCI 136
+//ASSIGNMENT 3D
+//REVERSE THAT ORDER SONN
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+	ifstream fin("Current_Reservoir_Levels.tsv");
+
+	if(fin.fail())
+	{
+		cout << "Failed to open the file." << endl; // exit if failed to open the file : <cstdlib>
+		exit(1);
+	}
+
+	string ONE;
+	getline(fin, ONE);
+
+	string date, earlier_date, later_date;
+	string eastSt, eastEl, westSt, westEl;
+
+	cout << "Enter an earlier date(MM/DD/2016): "; //asks user for input
+	cin >> earlier_date;
+
+	cout << "Enter an later date(MM/DD/2016): "; //asks user for input
+	cin >> later_date;
+
+	string myArray[400];
+	int center = 0;
+
+    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl)
+    {
+		if(date >= earlier_date && date <= later_date)
+		{	
+			myArray[center] = date + " " + westEl;
+			center++;
+		}
+	}	
+
+	for(int i = center - 1; i >= 0; i--)
+		cout << myArray[i] <<endl;
+
+	fin.close();
+
+return 0;
+}
